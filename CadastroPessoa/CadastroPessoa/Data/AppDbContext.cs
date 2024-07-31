deb99374e7d985ac70fb0e478736a822dfbe3bc0
Otávio <otaviotadeu244@gmail.com>
2024-07-31 13:58:44 -0300

    Add files via upload

---
+++ CadastroPessoa/CadastroPessoa/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CadastroPessoa.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroPessoa.Data;
+
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    DbSet<Pessoa> Pessoas { get; set; } = null!;
+}
+
---
+++ CadastroPessoa/CadastroPessoa/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace CadastroPessoa.Models;
+
+public class Pessoa
+{
+    public int id { get; set; }
+    public string nome { get; set; } = string.Empty;
+    public string cpf { get; set; } = string.Empty;
+    public string email { get; set; } = string.Empty;
+
+}
